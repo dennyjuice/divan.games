29ae513ec427121fe8c34560c635095a70000488
Denis <dennyjuice@gmail.com>
2020-12-10 00:46:18 +0700

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @ts-ignore
+import { ThemeProvider, createTheme, Arwes, Button } from 'arwes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme()}>
+      <Arwes>
+        <div style={{ padding: 20 }}>
+          <Button>My Button</Button>
+        </div>
+      </Arwes>
+    </ThemeProvider>
   );
 }
 
